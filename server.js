28eff3599a4639d6d089fd635e0c333f48d0e046
ifeoluwa <youremail@example.com>
2025-10-27 13:17:23 +0100

    Initial project setup with dependencies installed

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => connectDB(),
+console.log(`Server running on port ${PORT}`));
