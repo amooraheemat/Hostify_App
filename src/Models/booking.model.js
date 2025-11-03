d97309f4aed21dfacfc643e70fab764f6d223078
Clerk <kobrynclerk@gmail.com>
2025-11-03 04:13:39 +0100

    commit changes

--- src/Models/booking.model.js
+++ src/Models/booking.model.js
@@ -1,1 +1,43 @@
-_-export default mongoose.model("Booking", bookingSchema);
+
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    customerName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phoneNum: {
+      type: String,
+      required: true,
+    },
+    space: {
+      type: String,
+      enum: ["VIP", "Regular", "Outdoor"],
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    bookingStatus: {
+      type: String,
+      enum: ["Pending", "Confirmed", "Cancelled"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Booking = mongoose.model("Booking", bookingSchema);
+export default Booking;
