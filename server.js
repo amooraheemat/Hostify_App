5c575c339a22750d4afe0d02b0d523fe32a66b01
ifeoluwa <youremail@example.com>
2025-11-04 10:35:56 +0100

    arrangement

--- server.js
+++ server.js
@@ -19,3 +19,23 @@ app.use('/api/', userbook)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => connectDatabase(),
 console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+
+
+// MONGODB_URI="mongodb+srv://hostify:hostify@cluster0.p3hzj7i.mongodb.net/?appName=Cluster0"
+
+// PORT=5000
+// JWT_SECRET=dsljlvmdvldjjojlmlddl&^&$%$#^^*&mnbjgj&243354666BMNBVJ^&*%^$%
+// JWT_EXPIRES_IN=30d
+
+// # Email (optional)
+// EMAIL_HOST=smtp.gmail.com
+// EMAIL_PORT=465
+// EMAIL_USER=techkrush@gmail.com
+// EMAIL_PASS=ewxk qpnn sdyv pxip
