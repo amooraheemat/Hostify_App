bdf1c9c4b2aff3f958db32d84c11a954b61283da
Eli <oelimuyah@gmail.com>
2025-11-04 23:40:01 -0600

    changed JWT duration

--- backend/Controllers/auth.controller.js
+++ backend/Controllers/auth.controller.js
@@ -8,7 +8,7 @@ const generateToken = (user) => {
   return jwt.sign(
     { id: user._id, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: "1d" }
+    { expiresIn: "2h" }
   );
 };
 
