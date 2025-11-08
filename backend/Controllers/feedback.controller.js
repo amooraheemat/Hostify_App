acad62e698a4c110eae438279b91046636f07d66
amooraheemat <omotayoamoo29@gmail.com>
2025-11-08 22:56:24 +0100

    feedback controller

--- backend/Controllers/feedback.controller.js
+++ backend/Controllers/feedback.controller.js
@@ -24,7 +24,7 @@ export const getAllFeedback = async (req, res, next) => {
 // Delete Feedback (Admin)
 export const deleteFeedback = async (req, res, next) => {
   try {
-    const feedback = await Feedback.findByIdAndDelete(req.user.id);
+    const feedback = await Feedback.findByIdAndDelete(req.params.id);
 
     if (!feedback)
       return res.status(404).json({ message: "Feedback not found" });
