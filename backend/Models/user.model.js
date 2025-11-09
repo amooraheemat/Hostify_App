7b82de171b5c710d25964aab70c8bdc9c05015c1
amooraheemat <omotayoamoo29@gmail.com>
2025-11-09 22:38:17 +0100

    email: unique for user model

--- backend/Models/user.model.js
+++ backend/Models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
     username: { type: String, required: true, unique: true, trim: true },
-    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    email: { type: String, required: true, /*unique: true, */lowercase: true, trim: true },
     password: { type: String, required: true, minlength: 6 },
     phone: { type: String, required: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
